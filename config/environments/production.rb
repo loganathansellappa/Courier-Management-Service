b72cc4f8a3a5a0ae23bf95962482c597c973ef0e
loganathan <loganathan.ms@gmail.com>
2012-01-27 16:43:43 +0530

    git ignore file updated for active admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,5 @@ Courier::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+	config.action_mailer.default_url_options = {:host => 'http://couriermanagementservice.heroku.com'}
 end
